6d8e3ff5b71e8cecfc0320b3e56a7f19f5aabcc3
Aureum01 <71108402+Aureum01@users.noreply.github.com>
2023-05-16 10:30:03 -0400

    Create setup.py
    Signed-off-by: Aureum01 <71108402+Aureum01@users.noreply.github.com>

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+setup(
+    name="smapql",
+    version="1.0.0",
+    author="Aureum01",
+    author_email="futuresuperpilot@gmail.com",
+    description="A tool for discovering and testing SQL injection vulnerabilities in sitemaps",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aureum01/smapql",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10.11",
+)
